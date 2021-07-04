eee4b99ee18d4acfe078162404c7b933a728de71
lqxyz <liuqunxyz@gmail.com>
2021-07-04 13:45:11 +0100

    update run scripts to plot all the figures

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Plot time series of delta O18
+octave plotfit_d18o.m
+
+# Plot warm/cool stack figure from different proxies
+octave plot_stack_figure.m
+
+# Plot warm window
+# matlab -nodisplay < split.m
+octave split.m 
+
 # Compare the histogram from Matlab and Fortran results
 octave hist_MC_MCc.m
 
 # Compare the runtime between Matlab and Fortran (single or multiple cores)
 octave MC_time_comparison.m 
+
